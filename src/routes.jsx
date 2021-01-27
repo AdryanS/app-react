4289dd527d26cfcf897d5f30f859a53c5b333ad1
AdryanS <adryx7000@gmail.com>
2021-01-27 01:41:06 -0300

    version 0.0002

---
+++ src/routes.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import {Route, Switch} from 'wouter';
+import Login from './components/pages/login';
+import Criar from './components/pages/criarConta';
+import Home from './components/pages/home';
+import lives from './components/pages/lives/lives';
+import filmes from './components/pages/filmes';
+import erro404 from './components/pages/404'
+import filmes_categoria from './components/pages/filmes_categoria'
+import filme_info from './components/pages/filme_Info'
+
+function Router() {
+  return (
+    <Switch>
+      <Route exact path="/" component={Login}/>
+      <Route path="/Criar-Conta" component={Criar}/>
+      <Route path="/home" component={Home}/>
+      <Route path="/lives" component={lives}/>
+      <Route exact path="/filmes" component={filmes} />
+      <Route path="/filmes/:categoria" component={filmes_categoria} />
+      <Route component={erro404} />
+      <Route exact path="/filme" />
+      <Route path="/filme/:id" component={filme_info}/>
+    </Switch>
+  )
+}
+
+export default Router;
